a3d28a1e60ffddd53e963dd935fc1c877f989349
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-28 17:55:55 -0700

    Remarkable

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -1,25 +1,43 @@
 package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
-
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+		String riddle = JOptionPane.showInputDialog(null,
+				"If you have me, you want to share me.\n If you share me, you haven't got me. \nWhat am I? ");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by
+		// one
+		if (riddle.equalsIgnoreCase("Secret")) {
+			JOptionPane.showMessageDialog(null, "Correct!", riddle, score + 1);
+			score += 1;
 
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong.\n Secret");
 
-		// 5. Otherwise, say "wrong" and tell them the answer
+			// 5. Otherwise, say "wrong" and tell them the answer
 
-		// 6. Add some more riddles
+			// 6. Add some more riddles
+			String riddle2 = JOptionPane.showInputDialog(" What gets broken without being held? ");
 
-		// 2. Make a pop up to show the score.
-		
+			if (riddle2.equalsIgnoreCase("Promise")) {
+				JOptionPane.showMessageDialog(null, "Correct!", riddle2, score + 1);
+				score += 1;
+
+			} else {
+				JOptionPane.showMessageDialog(null, "Wrong.\n Promise");
+
+				// 2. Make a pop up to show the score.
+				JOptionPane.showMessageDialog(null, score);
+			}
+		}
 	}
 }
-
--- src/ifs_ints_and_loops/XGeeks.java
+++ src/ifs_ints_and_loops/XGeeks.java
@@ -1,26 +1,39 @@
 package ifs_ints_and_loops;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
+
 //    Level 0
-
-
-
 
 import javax.swing.JOptionPane;
 
 /*
-* Everyone has a superpower. Mine is writing recipes. This program will store
-* the superpowers of all the people in the classroom. E.g. When I type "June",
-* your program should say "June's superpower is writing recipes".
+* Everyone has a super power. Mine is writing recipes. This program will store
+* the super powers of all the people in the classroom. E.g. When I type "June",
+* your program should say "June's super power is writing recipes".
 */
 public class XGeeks {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
-		// 1. Save the superpower for each person in a variable.
-
+		// 1. Save the super power for each person in a variable.
+		String superpower = JOptionPane.showInputDialog(null," Enter a super power. ");
+		String superpower1 = JOptionPane.showInputDialog(null,"Enter a super power. ");
+		String superpower2 = JOptionPane.showInputDialog(null, "Enter a super power. ");
 		// 2. Ask the user to enter a name. Store their answer in a variable.
+		String name = JOptionPane.showInputDialog(null, "Enter your name.");
+		// 3. Show the super power in a pop-up, depending on the name entered.
+		if (name.equalsIgnoreCase("Superman")) {
+			JOptionPane.showMessageDialog(null, superpower);
 
-		// 3. Show the superpower in a pop-up, depending on the name entered. 
+		} 		
+		else if (name.equalsIgnoreCase("Flash")) {
+			JOptionPane.showMessageDialog(null, superpower1);
 
+		}
+		else if (name.equalsIgnoreCase("Poseidon")) {
+			JOptionPane.showMessageDialog(null, superpower2);
+		}
+		
+		else {
+			JOptionPane.showMessageDialog(null, "You shall stay mortal!!!");
+		}
 	}
 }
-
