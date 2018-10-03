c5e8acc25ef92d7d05d51f0bb3ecbfa390972256
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-02 18:04:31 -0700

    Variables worksheet, for loops

--- src/ifs_ints_and_loops/RobotInSpace.java
+++ src/ifs_ints_and_loops/RobotInSpace.java
@@ -16,23 +16,35 @@ public class RobotInSpace implements KeyEventDispatcher {
 	/*
 	 * Make the Robot move around the screen when the arrow keys are pressed...
 	 * 
-	 * 1. IMPORTANT: For this recipe, use rob.microMove(distance) to move your
-	 * Robot and rob.setAngle(angle) to change the direction of your Robot. //Do
-	 * not add code here - go to step 2
+	 * 1. IMPORTANT: For this recipe, use rob.microMove(distance) to move your Robot
+	 * and rob.setAngle(angle) to change the direction of your Robot. //Do not add
+	 * code here - go to step 2
 	 */
 
 	private void moveRobot(int keyPressed) throws InterruptedException {
 		// 2. Print out the keyPressed variable and write down the numbers for
 		// each arrow key
+		System.out.println(keyPressed);
+		if (keyPressed == 38) {
 
-		// 3. If the up arrow is pressed, move the Robot up the screen.
-
-		// 4. If the down arrow is pressed, move the Robot down.
+			// 3. If the up arrow is pressed, move the Robot up the screen.
+			rob.setAngle(0);
+			rob.microMove(5);
+		} else if (keyPressed == 40) {
+			// 4. If the down arrow is pressed, move the Robot down.
+			rob.setAngle(180);
+			rob.microMove(5);
+		}
 
 		// 5. If the left arrow is pressed, make the Robot go left.
-
-		// 6. If right is pressed, move the Robot right.
-
+		else if (keyPressed == 37) {
+			rob.setAngle(270);
+			rob.microMove(5);
+		} else if (keyPressed == 39) {
+			// 6. If right is pressed, move the Robot right.
+			rob.setAngle(90);
+			rob.microMove(5);
+		}
 		// 7. Run your program and move the Robot to RD-2D for a surprise!
 	}
 
